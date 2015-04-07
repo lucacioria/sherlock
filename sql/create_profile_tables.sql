5feeeb3b9f1de71117864456c389f7d8dad5acf2
Luca Cioria <vircheck@gmail.com>
2015-04-07 05:28:56 +0200

    fix typo

--- sql/create_profile_tables.sql
+++ sql/create_profile_tables.sql
@@ -11,7 +11,7 @@ CREATE TABLE `profiles` (
   `profile_kind_id` int(11) DEFAULT NULL,
   `user_id` varchar(45) NOT NULL,
   `month` char(7) NOT NULL,
-  `hisotogram` text NOT NULL,
+  `histogram` text NOT NULL,
   KEY `user_id` (`user_id`),
   KEY `profile_kind_id` (`profile_kind_id`)
 );
